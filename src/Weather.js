e3f5667ae4b92cfe7d7adf3d9ce1094036d6b641
dagifer <125514178+dagifer@users.noreply.github.com>
2023-07-11 17:16:20 +0100

    modify the current project app

---
+++ src/Weather.js
@@ -0,0 +1,65 @@
+import React, { useState } from "react";
+import axios from "axios";
+import WeatherTab from "./WeatherTab";
+import "./Weather.css";
+
+export default function Weather(props) {
+  const [ready, setReady] = useState(false);
+  const [weatherData, setWeatherData] = useState({});
+  const [city, setCity] = useState(props.defaultCity);
+  function handleResponse(response) {
+    console.log(response.data);
+    setWeatherData({
+      temperature: response.data.main.temp,
+      city: response.data.name,
+      humidity: response.data.main.humidity,
+      wind: response.data.wind.speed,
+      date: new Date(response.data.dt * 1000),
+      description: response.data.weather[0].description,
+      iconUrl: `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`,
+    });
+    setReady(true);
+  }
+
+  function search() {
+    const apiKey = "a969311cfcbb4a83dfad2cf7478397f9";
+    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(handleResponse);
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    search();
+  }
+
+  function showCityChange(event) {
+    setCity(event.target.value);
+  }
+  if (ready) {
+    return (
+      <div className="Weather">
+        <form onSubmit={handleSubmit}>
+          <div className="row">
+            <div className="col-9">
+              <input
+                type="search"
+                placeholder="Enter your city"
+                className="form-control"
+                autoFocus="on"
+                onChange={showCityChange}
+              />
+            </div>
+            <div className="col-3">
+              <input type="submit" value="search" className="btn btn-primary" />
+            </div>
+          </div>
+        </form>
+        <WeatherTab data={weatherData} />
+      </div>
+    );
+  } else {
+    search();
+
+    return "Loading ...";
+  }
+}
---
+++ src/WeatherTab.js
@@ -0,0 +1,36 @@
+import React from "react";
+import FormattedDate from "./FormattedDate";
+import WeatherTemperature from "./WeatherTemperature";
+
+export default function WeatherTab(props) {
+  return (
+    <div className="WeatherInfo">
+      <h1>{props.data.city}</h1>
+      <ul>
+        <li>
+          <FormattedDate date={props.data.date} />
+        </li>
+        <li className="text-capitalize">{props.data.description}</li>
+      </ul>
+      <div className="row">
+        <div className="col-6">
+          <div className="clearfix d-flex align-item-center">
+            <div className="float-left">
+              <img src={props.data.iconUrl} alt={props.data.description} />
+            </div>
+            <div className="float-left">
+              <WeatherTemperature celsius={props.data.temperature} />
+            </div>
+          </div>
+        </div>
+
+        <div className="col-6">
+          <ul>
+            <li>Humidity: {Math.round(props.data.humidity)}%</li>
+            <li>Wind:{Math.round(props.data.wind)}km/h</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+}
